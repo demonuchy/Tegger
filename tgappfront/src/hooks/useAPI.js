04b9d6a0216565b18ed9c8fdda5f339642f2d24e
demonuchy <demonuchy@gmail.com>
2025-11-14 15:17:23 +0300

    COMMIT BeregDonaApp v1.2

--- tgappfront/src/hooks/useAPI.js
+++ tgappfront/src/hooks/useAPI.js
@@ -1,17 +1,17 @@
 
-const domain = "https://bdapi.loca.lt"
+const domain = process.env.SERVER_HOST || 'https://bdapi.loca.lt'
 
 // В вашем useAPI.js
 const useApi = () => {
     const aplicationRequest = async (fullName, telegramId, telegramUserName, phoneNumber) => {
       try {
-        const response = await fetch(`${domain}/api/auth/application`, {
+        const response = await fetch(`${domain}/api/application`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
           mode: 'cors', // Явно указываем CORS mode
-        credentials: 'omit',
+          credentials: 'omit',
           body: JSON.stringify({
             full_name: fullName,
             telegram_id: telegramId,
